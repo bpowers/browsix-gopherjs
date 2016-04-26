9588beb324b014c178d01752e87930f554db9535
Bobby Powers <bobbypowers@gmail.com>
2016-04-26 13:47:36 -0400

    prelude/goroutines: setImmediate polyfill that works in Workers
    in Firefox, the message port is way slower -- only fall back to that
    in Chrome.

--- compiler/prelude/goroutines.go
+++ compiler/prelude/goroutines.go
@@ -159,58 +159,189 @@ var $go = function(fun, args, direct) {
   $schedule($goroutine, direct);
 };
 
-(function() {
-  var _this = this;
-  if (typeof setImmediate === 'undefined') {
-    var g = self;
-    var timeouts = [];
-    var messageName = "zero-timeout-message";
-    var canUsePostMessage = function () {
-      if (typeof g.importScripts !== 'undefined' || !g.postMessage)
-        return false;
-      var isAsync = true;
-      var oldOnMessage = g.onmessage;
-      g.onmessage = function () { isAsync = false; };
-      g.postMessage('', '*');
-      g.onmessage = oldOnMessage;
-      return isAsync;
-    };
-    if (canUsePostMessage()) {
-      g.setImmediate = function (fn) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-          args[_i - 1] = arguments[_i];
-        }
-        timeouts.push([fn, args]);
-        g.postMessage(messageName, "*");
-      };
-      var handleMessage = function (event) {
-        if (event.source === self && event.data === messageName) {
-          if (event.stopPropagation)
-            event.stopPropagation();
-          else
-            event.cancelBubble = true;
-        }
-        if (timeouts.length > 0) {
-          var _a = timeouts.shift(), fn = _a[0], args = _a[1];
-          return fn.apply(_this, args);
-        }
-      };
-      g.addEventListener('message', handleMessage, true);
-      console.log('using postMessage for setImmediate');
+// from https://github.com/YuzuJS/setImmediate/blob/master/setImmediate.js
+// Copyright (c) 2012 Barnesandnoble.com, llc, Donavon West, and Domenic Denicola
+// MIT licensed, see https://github.com/YuzuJS/setImmediate/blob/master/LICENSE.txt
+(function (global, undefined) {
+    "use strict";
+
+    if (global.setImmediate) {
+        return;
     }
-    else {
-      console.log('not using postMessage for setImmediate :(');
-      g.setImmediate = function (fn) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-          args[_i - 1] = arguments[_i];
-        }
-        return setTimeout.apply(_this, [fn, 0].concat(args));
-      };
+
+    var nextHandle = 1; // Spec says greater than zero
+    var tasksByHandle = {};
+    var currentlyRunningATask = false;
+    var doc = global.document;
+    var setImmediate;
+
+    function addFromSetImmediateArguments(args) {
+        tasksByHandle[nextHandle] = partiallyApplied.apply(undefined, args);
+        return nextHandle++;
     }
-  }
-})();
+
+    // This function accepts the same arguments as setImmediate, but
+    // returns a function that requires no arguments.
+    function partiallyApplied(handler) {
+        var args = [].slice.call(arguments, 1);
+        return function() {
+            if (typeof handler === "function") {
+                handler.apply(undefined, args);
+            } else {
+                (new Function("" + handler))();
+            }
+        };
+    }
+
+    function runIfPresent(handle) {
+        // From the spec: "Wait until any invocations of this algorithm started before this one have completed."
+        // So if we're currently running a task, we'll need to delay this invocation.
+        if (currentlyRunningATask) {
+            // Delay by doing a setTimeout. setImmediate was tried instead, but in Firefox 7 it generated a
+            // "too much recursion" error.
+            setTimeout(partiallyApplied(runIfPresent, handle), 0);
+        } else {
+            var task = tasksByHandle[handle];
+            if (task) {
+                currentlyRunningATask = true;
+                try {
+                    task();
+                } finally {
+                    clearImmediate(handle);
+                    currentlyRunningATask = false;
+                }
+            }
+        }
+    }
+
+    function clearImmediate(handle) {
+        delete tasksByHandle[handle];
+    }
+
+    function installNextTickImplementation() {
+        setImmediate = function() {
+            var handle = addFromSetImmediateArguments(arguments);
+            process.nextTick(partiallyApplied(runIfPresent, handle));
+            return handle;
+        };
+    }
+
+    function canUsePostMessage() {
+        // The test against importScripts prevents this implementation from being installed inside a web worker,
+        // where global.postMessage means something completely different and can't be used for this purpose.
+        if (global.postMessage && !global.importScripts) {
+            var postMessageIsAsynchronous = true;
+            var oldOnMessage = global.onmessage;
+            global.onmessage = function() {
+                postMessageIsAsynchronous = false;
+            };
+            global.postMessage("", "*");
+            global.onmessage = oldOnMessage;
+            return postMessageIsAsynchronous;
+        }
+    }
+
+    function installPostMessageImplementation() {
+        // Installs an event handler on global for the message event: see
+        // * https://developer.mozilla.org/en/DOM/window.postMessage
+        // * http://www.whatwg.org/specs/web-apps/current-work/multipage/comms.html#crossDocumentMessages
+
+        var messagePrefix = "setImmediate$" + Math.random() + "$";
+        var onGlobalMessage = function(event) {
+            if (event.source === global &&
+                typeof event.data === "string" &&
+                event.data.indexOf(messagePrefix) === 0) {
+                runIfPresent(+event.data.slice(messagePrefix.length));
+            }
+        };
+
+        if (global.addEventListener) {
+            global.addEventListener("message", onGlobalMessage, false);
+        } else {
+            global.attachEvent("onmessage", onGlobalMessage);
+        }
+
+        setImmediate = function() {
+            var handle = addFromSetImmediateArguments(arguments);
+            global.postMessage(messagePrefix + handle, "*");
+            return handle;
+        };
+    }
+
+    function installMessageChannelImplementation() {
+        var channel = new MessageChannel();
+        channel.port1.onmessage = function(event) {
+            var handle = event.data;
+            runIfPresent(handle);
+        };
+
+        setImmediate = function() {
+            var handle = addFromSetImmediateArguments(arguments);
+            channel.port2.postMessage(handle);
+            return handle;
+        };
+    }
+
+    function installReadyStateChangeImplementation() {
+        var html = doc.documentElement;
+        setImmediate = function() {
+            var handle = addFromSetImmediateArguments(arguments);
+            // Create a <script> element; its readystatechange event will be fired asynchronously once it is inserted
+            // into the document. Do so, thus queuing up the task. Remember to clean up once it's been called.
+            var script = doc.createElement("script");
+            script.onreadystatechange = function () {
+                runIfPresent(handle);
+                script.onreadystatechange = null;
+                html.removeChild(script);
+                script = null;
+            };
+            html.appendChild(script);
+            return handle;
+        };
+    }
+
+    function installSetTimeoutImplementation() {
+        setImmediate = function() {
+            var handle = addFromSetImmediateArguments(arguments);
+            setTimeout(partiallyApplied(runIfPresent, handle), 0);
+            return handle;
+        };
+    }
+
+    // If supported, we should attach to the prototype of global, since that is where setTimeout et al. live.
+    var attachTo = Object.getPrototypeOf && Object.getPrototypeOf(global);
+    attachTo = attachTo && attachTo.setTimeout ? attachTo : global;
+
+    var IS_CHROME =
+	typeof navigator !== 'undefined' &&
+	navigator.userAgent.match(/Chrome/) &&
+	!navigator.userAgent.match(/Edge/);
+
+    // Don't get fooled by e.g. browserify environments.
+    if ({}.toString.call(global.process) === "[object process]") {
+        // For Node.js before 0.9
+        installNextTickImplementation();
+
+    } else if (canUsePostMessage()) {
+        // For non-IE10 modern browsers
+        installPostMessageImplementation();
+
+    } else if (IS_CHROME && global.MessageChannel) {
+        // For web workers, where supported
+        installMessageChannelImplementation();
+
+    } else if (doc && "onreadystatechange" in doc.createElement("script")) {
+        // For IE 6–8
+        installReadyStateChangeImplementation();
+
+    } else {
+        // For older browsers
+        installSetTimeoutImplementation();
+    }
+
+    attachTo.setImmediate = setImmediate;
+    attachTo.clearImmediate = clearImmediate;
+}(typeof self === "undefined" ? typeof global === "undefined" ? this : global : self));
 
 var $scheduled = [], $schedulerActive = false;
 var $runScheduled = function() {
